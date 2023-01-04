6aba88c0239954b0195fbd0360013e0281e6ca32
Pavel Kukayeu <pavelkukayeu@gmail.com>
2023-01-04 22:55:40 +0300

    add:play button

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,7 +5,11 @@ import {useAppDispatch} from '../hooks/useAppDispatch'
 import {Bird} from './Bird'
 import {Pipe} from './Pipe'
 import styles from './Game.module.css'
-import {changeWidthScreenAC} from '../store/pipeReducer';
+import {changeWidthScreenAC} from '../store/pipeReducer'
+import flappyBird from '../../public/image/flappyBird.png'
+import gameOver from '../../public/image/gameOver.png'
+import play from '../../public/image/play.png'
+import Image from 'next/image'
 
 let gameLoop
 let pipeGenerator
@@ -25,27 +29,29 @@ export const Game = () => {
 
   useEffect(() => {
     const handleKeyPress = (e) => {
-      if (e.keyCode === 32) {
+      if (e.keyCode === 32 && status === 'playing') {
         fly()
-      }
-      if (status !== 'playing') {
-        start(status)
       }
     }
     const handleClick = (e) => {
-      fly()
-      if (status !== 'playing') {
-        start(status)
+      if( status === 'playing') {
+        fly()
       }
     }
 
-    document.addEventListener('keypress',handleKeyPress)
-    document.addEventListener('click',handleClick)
+    document.addEventListener('keypress', handleKeyPress)
+    document.addEventListener('click', handleClick)
     return () => {
-      document.removeEventListener('keypress',handleKeyPress)
-      document.removeEventListener('click',handleClick)
+      document.removeEventListener('keypress', handleKeyPress)
+      document.removeEventListener('click', handleClick)
     }
   }, [status])
+
+  const handleStartGame = () => {
+    if (status !== 'playing') {
+      start(status)
+    }
+  }
 
   const birdYRef = useRef(null)
   const xRef = useRef(null)
@@ -66,12 +72,11 @@ export const Game = () => {
   useEffect(() => {
     const screenWidth = window ? window.screen.width : 390
     dispatch(changeWidthScreenAC(screenWidth))
-  } , [])
-
+  }, [])
 
   const start = (status) => {
     if (status !== 'playing') {
-       gameLoop = setInterval(() => {
+      gameLoop = setInterval(() => {
         dispatch({type: 'FALL'})
         dispatch({type: 'RUNNING'})
 
@@ -106,14 +111,16 @@ export const Game = () => {
         }
       })
 
-    if (birdY > 512 - 108 || birdY < 0) {
+    if (birdY > 512 - 134 || birdY < 0) {
       dispatch({type: 'GAME_OVER'})
     }
 
     if (challenge.length) {
       const {x1, y1, x2, y2} = challenge[0]
 
-      if ((x1 < 120 && 120 < x1 + 52 && birdY < y1) || (x2 < 120 && 120 < x2 + 52 && birdY > y2)) {
+      console.log(x1, y1, x2, y2)
+
+      if ((x1 < 120 && 120 < x1 + 52 && birdY < y1 ) || (x2 < 120 && 120 < x2 + 52 && birdY > y2 - 36)) {
         dispatch({type: 'GAME_OVER'})
       }
     }
@@ -121,7 +128,37 @@ export const Game = () => {
 
   return (
     <div className={styles.game}>
-      <Bird />
+      {status === 'playing' ? (
+        <Bird />
+      ) : status === 'game-over' ? (
+        <>
+          <div className={styles.title}>
+            <Image src={gameOver} width={250} height={50} alt={'Flappy bird'} />
+            <button onClick={handleStartGame} style={{position: 'relative', left: '75px', top: '50px', background: "none", border: "0"}}>
+              <Image
+                  src={play}
+                  width={100}
+                  height={50}
+                  alt={'Play'}
+              />
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.title}>
+            <Image src={flappyBird} width={250} height={50} alt={'Game over'} />
+            <button onClick={handleStartGame} style={{position: 'relative', left: '75px', top: '50px', background: "none", border: "0"}}>
+              <Image
+                  src={play}
+                  width={100}
+                  height={50}
+                  alt={'Play'}
+              />
+            </button>
+          </div>
+        </>
+      )}
       <Pipe />
       <Foreground />
     </div>
